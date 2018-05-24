2a4692337a6dd82d75249929c83d2a513f5f3eab
kilcannon <scott.bouchey@gmail.com>
2018-05-24 14:52:03 -0700

    finished core functionality of app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
+var friendsArray =  [
 {
-  "name":"Link",
-  "photo":"https://www.ssbwiki.com/images/thumb/a/a8/Link_Artwork.png/440px-Link_Artwork.png",
-  "scores":[
+  name: "Link",
+  photo:"https://www.ssbwiki.com/images/thumb/a/a8/Link_Artwork.png/440px-Link_Artwork.png",
+  scores:[
       5,
       1,
       4,
@@ -12,13 +13,14 @@
       5,
       4,
       1
-    ]
-}
+    ],
+    total: 32
+},
 
 {
-  "name":"Luigi",
-  "photo":"https://www.ssbwiki.com/images/thumb/5/5c/Luigi_MP10.png/400px-Luigi_MP10.png",
-  "scores":[
+  name:"Luigi",
+  photo:"https://www.ssbwiki.com/images/thumb/5/5c/Luigi_MP10.png/400px-Luigi_MP10.png",
+  scores:[
       2,
       2,
       2,
@@ -29,13 +31,14 @@
       2,
       2,
       2
-    ]
-}
+    ],
+    total: 20
+},
 
 {
-  "name":"Kirby",
-  "photo":"https://www.ssbwiki.com/images/thumb/6/63/Kirby_KSA.png/550px-Kirby_KSA.png",
-  "scores":[
+  name:"Kirby",
+  photo:"https://www.ssbwiki.com/images/thumb/6/63/Kirby_KSA.png/550px-Kirby_KSA.png",
+  scores:[
       3,
       3,
       3,
@@ -46,13 +49,14 @@
       3,
       3,
       3
-    ]
-}
+    ],
+    total: 30
+},
 
 {
-  "name":"Pikachu",
-  "photo":"https://www.ssbwiki.com/images/0/0f/Pikachu_FireRed_LeafGreen.png",
-  "scores":[
+  name:"Pikachu",
+  photo:"https://www.ssbwiki.com/images/0/0f/Pikachu_FireRed_LeafGreen.png",
+  scores:[
       4,
       4,
       4,
@@ -63,13 +67,14 @@
       4,
       4,
       4
-    ]
-}
+    ],
+    total: 40
+},
 
 {
-  "name":"Bowser",
-  "photo":"https://www.ssbwiki.com/images/thumb/6/68/Bowser_MP10.png/500px-Bowser_MP10.png",
-  "scores":[
+  name:"Bowser",
+  photo:"https://www.ssbwiki.com/images/thumb/6/68/Bowser_MP10.png/500px-Bowser_MP10.png",
+  scores:[
       5,
       5,
       5,
@@ -80,13 +85,14 @@
       5,
       5,
       5
-    ]
-}
+    ],
+    total: 50
+},
 
 {
-  "name":"Mega Man",
-  "photo":"https://www.ssbwiki.com/images/a/a6/MM10-MegaMan.png",
-  "scores":[
+  name:"Mega Man",
+  photo:"https://www.ssbwiki.com/images/a/a6/MM10-MegaMan.png",
+  scores:[
       1,
       1,
       1,
@@ -97,30 +103,32 @@
       1,
       1,
       1
-    ]
-}
+    ],
+    total: 10
+},
 
 {
-  "name":"Samus Aran",
-  "photo":"https://www.ssbwiki.com/images/thumb/6/68/Samus_Other_M_Artwork.png/500px-Samus_Other_M_Artwork.png",
-  "scores":[
+  name:"Samus Aran",
+  photo:"https://www.ssbwiki.com/images/thumb/6/68/Samus_Other_M_Artwork.png/500px-Samus_Other_M_Artwork.png",
+  scores:[
       5,
       1,
       5,
       1,
-      5,
+      3,
       1,
       5,
       1,
       5,
       1
-    ]
-}
+    ],
+    total: 28
+},
 
 {
-  "name":"Jigglypuff",
-  "photo":"https://www.ssbwiki.com/images/thumb/0/0c/Jigglypuff_FireRed_LeafGreen.png/550px-Jigglypuff_FireRed_LeafGreen.png",
-  "scores":[
+  name:"Jigglypuff",
+  photo:"https://www.ssbwiki.com/images/thumb/0/0c/Jigglypuff_FireRed_LeafGreen.png/550px-Jigglypuff_FireRed_LeafGreen.png",
+  scores:[
       2,
       3,
       2,
@@ -131,13 +139,14 @@
       3,
       2,
       3
-    ]
-}
+    ],
+    total: 25
+},
 
 {
-  "name":"Wario",
-  "photo":"https://www.ssbwiki.com/images/thumb/3/35/Wario_WarioWareGold.png/600px-Wario_WarioWareGold.png",
-  "scores":[
+  name:"Wario",
+  photo:"https://www.ssbwiki.com/images/thumb/3/35/Wario_WarioWareGold.png/600px-Wario_WarioWareGold.png",
+  scores:[
       3,
       4,
       3,
@@ -148,23 +157,28 @@
       4,
       3,
       4
-    ]
-}
+    ],
+    total: 35
+},
 
 {
-  "name":"Princess Peach",
-  "photo":"https://www.ssbwiki.com/images/thumb/e/e9/Peach_MP10.png/500px-Peach_MP10.png",
-  "scores":[
+  name:"Princess Peach",
+  photo:"https://www.ssbwiki.com/images/thumb/e/e9/Peach_MP10.png/500px-Peach_MP10.png",
+  scores:[
       2,
-      4,
+      1,
       2,
-      4,
+      1,
       2,
       3,
       1,
-      5,
-      4,
-      3
-    ]
+      1,
+      1,
+      1
+    ],
+    total: 15
 }
+]
+
+module.exports = friendsArray;
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,4 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends)
   })
-
-  app.post('/api/friends', function(req, res) {
-      friends.push(req.body);
-  })
 }
